3504f5849ebeabcc44c627d693df38f039dc97e0
James Monger <jameskmonger@hotmail.co.uk>
2016-03-13 19:42:14 +0000

    Fix symlink script

--- scripts/link.js
+++ scripts/link.js
@@ -5,7 +5,7 @@ var link = require("fs-symlink"),
     root = path.resolve(__dirname + "/../");
 
 console.log("Creating tidyfolders symlink");
-link(root + "src", root + "node_modules/tidyfolders", "junction")
+link(root + "/src", root + "/node_modules/tidyfolders", "junction")
     .then(function () {
         console.log("Symlink created successfully.");
     })
