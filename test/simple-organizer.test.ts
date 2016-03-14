fce419a2048027870c1e24be319fc394760ff0d9
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:26:39 +0000

    Store expected result

--- test/simple-organizer.test.ts
+++ test/simple-organizer.test.ts
@@ -8,44 +8,48 @@ import { DirectoryModelBuilder } from './_builders/directory-model.builder';
 
 test('directory "james" returns "j"', t => {
     let directoryName = 'james';
+    let expectedContainingDirectory = 'j';
 
     t.plan(1);
 
     let organizer = new SimpleOrganizer();
     let model = new DirectoryModelBuilder().withName(directoryName).build();
 
-    t.equal(organizer.getContainingDirectory(model), 'j');
+    t.equal(organizer.getContainingDirectory(model), expectedContainingDirectory);
 });
 
 test('directory "JAMES" returns "j"', t => {
     let directoryName = 'JAMES';
+    let expectedContainingDirectory = 'j';
 
     t.plan(1);
 
     let organizer = new SimpleOrganizer();
     let model = new DirectoryModelBuilder().withName(directoryName).build();
 
-    t.equal(organizer.getContainingDirectory(model), 'j');
+    t.equal(organizer.getContainingDirectory(model), expectedContainingDirectory);
 });
 
 test('directory "apple" returns "a"', t => {
     let directoryName = 'apple';
+    let expectedContainingDirectory = 'a';
 
     t.plan(1);
 
     let organizer = new SimpleOrganizer();
     let model = new DirectoryModelBuilder().withName(directoryName).build();
 
-    t.equal(organizer.getContainingDirectory(model), 'a');
+    t.equal(organizer.getContainingDirectory(model), expectedContainingDirectory);
 });
 
 test('directory "APPLE" returns "a"', t => {
     let directoryName = 'APPLE';
+    let expectedContainingDirectory = 'a';
 
     t.plan(1);
 
     let organizer = new SimpleOrganizer();
     let model = new DirectoryModelBuilder().withName(directoryName).build();
 
-    t.equal(organizer.getContainingDirectory(model), 'a');
+    t.equal(organizer.getContainingDirectory(model), expectedContainingDirectory);
 });
