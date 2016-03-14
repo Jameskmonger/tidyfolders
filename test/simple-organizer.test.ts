2d9147a64131f244af6a3aa5f898b3f5ea460ba7
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:17:41 +0000

    Use model builder

--- test/simple-organizer.test.ts
+++ test/simple-organizer.test.ts
@@ -1,16 +1,17 @@
 ///<reference path="../typings/main.d.ts"/>
 
-import * as test from 'tape';
 import { SimpleOrganizer } from 'tidyfolders/simple-organizer';
 import { DirectoryModel } from 'tidyfolders/directory-model';
+
+import * as test from 'tape';
+import { DirectoryModelBuilder } from './_builders/directory-model.builder';
 
 let organizer = new SimpleOrganizer();
 
 test('directory "james" returns "j"', t => {
     t.plan(1);
 
-    let model = new DirectoryModel();
-    model.name = 'james';
+    let model = new DirectoryModelBuilder().withName('james').build();
 
     t.equal(organizer.getContainingDirectory(model), 'j');
 
