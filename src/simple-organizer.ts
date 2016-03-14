c9d66462b7ada378c2cc8978aefac840be8c4a4a
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:10:44 +0000

    Make SimpleOrganizer implement IOrganizer

--- src/simple-organizer.ts
+++ src/simple-organizer.ts
@@ -1,3 +1,10 @@
-export class SimpleOrganizer {
+import { IOrganizer } from './_interfaces/organizer.i';
+import { DirectoryModel } from './directory-model';
+
+export class SimpleOrganizer implements IOrganizer {
+
+    public getContainingDirectory(dir: DirectoryModel): string {
+        return '';
+    }
 
 }
