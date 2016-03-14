dbb1ea219320797b4825b7f05aada4a5f41d39de
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:15:08 +0000

    Create directory model builder

---
+++ test/_builders/directory-model.builder.ts
@@ -0,0 +1,9 @@
+import { DirectoryModel } from 'tidyfolders/directory-model';
+
+export class DirectoryModelBuilder {
+
+    public build(): DirectoryModel {
+        return new DirectoryModel();
+    }
+
+}
