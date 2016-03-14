e0d87230d4dffc3a24fc8dfac6aa762777ee3c22
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:21:05 +0000

    Implement IBuilder in SimpleOrganizerBuilder

--- test/_builders/simple-organizer.builder.ts
+++ test/_builders/simple-organizer.builder.ts
@@ -1,6 +1,7 @@
+import { IBuilder } from './_interfaces/builder.i';
 import { SimpleOrganizer } from 'tidyfolders/simple-organizer';
 
-export class SimpleOrganizerBuilder {
+export class SimpleOrganizerBuilder implements IBuilder<SimpleOrganizer> {
 
     public build(): SimpleOrganizer {
         return new SimpleOrganizer();
