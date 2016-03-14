7e3a1b5cbdb60d7562fbc20205c88ea05365b728
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:13:52 +0000

    Create first test

--- test/simple-organizer.test.ts
+++ test/simple-organizer.test.ts
@@ -1,8 +1,17 @@
 ///<reference path="../typings/main.d.ts"/>
 
 import * as test from 'tape';
+import { SimpleOrganizer } from 'tidyfolders/simple-organizer';
+import { DirectoryModel } from 'tidyfolders/directory-model';
 
-test('a', t => {
+let organizer = new SimpleOrganizer();
+
+test('directory "james" returns "j"', t => {
     t.plan(1);
-    t.equal(5, 1);
+
+    let model = new DirectoryModel();
+    model.name = 'james';
+
+    t.equal(organizer.getContainingDirectory(model), 'j');
+
 });
