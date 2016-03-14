3e2cb2ad9c96953650da071e3250e1d296e6d08a
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:25:38 +0000

    Pass simple organizer tests

--- src/simple-organizer.ts
+++ src/simple-organizer.ts
@@ -4,7 +4,7 @@ import { DirectoryModel } from './directory-model';
 export class SimpleOrganizer implements IOrganizer {
 
     public getContainingDirectory(dir: DirectoryModel): string {
-        return 'j';
+        return dir.name[0].toLowerCase();
     }
 
 }
