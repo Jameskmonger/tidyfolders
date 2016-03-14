08e739f8d32fa2adbe1133c4ab66217bf95cf794
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:15:59 +0000

    add withName to builder

--- test/_builders/directory-model.builder.ts
+++ test/_builders/directory-model.builder.ts
@@ -2,8 +2,15 @@ import { DirectoryModel } from 'tidyfolders/directory-model';
 
 export class DirectoryModelBuilder {
 
+    private _name;
+
     public build(): DirectoryModel {
         return new DirectoryModel();
+    }
+
+    public withName(name: string): DirectoryModelBuilder {
+        this._name = name;
+        return this;
     }
 
 }
