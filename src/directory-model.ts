7f28d76394592cf782f7792cd9d5ce0394cdc142
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 14:34:09 +0000

    Implement directorymodel getName and constructor

--- src/directory-model.ts
+++ src/directory-model.ts
@@ -1,5 +1,13 @@
 export class DirectoryModel {
 
-    name: string;
+    private _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    public getName(): string {
+        return this._name;
+    }
 
 }
