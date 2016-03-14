1efa60dabb9bd62c2febe3e98993710460c01c12
James Monger <jameskmonger@hotmail.co.uk>
2016-03-14 17:57:17 +0000

    Test proxyquire

--- src/file-system.ts
+++ src/file-system.ts
@@ -1,9 +1,14 @@
+///<reference path="../typings/main.d.ts"/>
+
 import { IFileSystem } from './_interfaces/file-system.i';
 import { DirectoryModel } from './directory-model';
+
+import * as fs from 'fs';
 
 export class FileSystem implements IFileSystem {
 
     public getAllDirectories(path: string): Array<DirectoryModel> {
+        (<any>fs).testMethod();
         return [];
     }
 
--- test/file-system.test.ts
+++ test/file-system.test.ts
@@ -1,5 +1,21 @@
 ///<reference path="../typings/main.d.ts"/>
 
-import { FileSystem } from 'tidyfolders/file-system';
-
 import * as test from 'tape';
+import * as proxyquire from 'proxyquire';
+
+test('it should call fs.readdirSync for path /', t => {
+    t.plan(1);
+
+    let nodeFsStub = {
+        testMethod: () => {
+            console.log('test proxyquire');
+        }
+    };
+
+    let FileSystem = proxyquire('tidyfolders/file-system', {'fs': nodeFsStub}).FileSystem;
+
+    let fileSystem = new FileSystem();
+    fileSystem.getAllDirectories('/');
+
+    t.equal(5, 5);
+});
