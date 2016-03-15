f5c11e6f20f0c48469c32f66fb13d806be5e8b03
James Monger <jameskmonger@hotmail.co.uk>
2016-03-15 13:41:46 +0000

    Add organizer parameter

--- src/tidyfolders-manager.ts
+++ src/tidyfolders-manager.ts
@@ -1,9 +1,11 @@
 import { IFileSystem } from './_interfaces/file-system.i';
+import { IOrganizer } from './_interfaces/organizer.i';
 
 export class TidyFoldersManager {
 
     constructor(
-        fileSystem: IFileSystem
+        fileSystem: IFileSystem,
+        organizer: IOrganizer
     ) {
         if (fileSystem === null || fileSystem === undefined) {
             throw Error("Dependency 'fileSystem' was null or undefined.");
--- test/tidyfolders-manager.test.ts
+++ test/tidyfolders-manager.test.ts
@@ -1,6 +1,7 @@
 ///<reference path="../typings/main.d.ts"/>
 
 import { TidyFoldersManager } from 'tidyfolders/tidyfolders-manager';
+import { SimpleOrganizerBuilder } from './_builders/simple-organizer.builder';
 
 import * as test from 'tape';
 
@@ -8,9 +9,10 @@ test('it throws Error if undefined fileSystem', t => {
     t.plan(1);
 
     let providedFileSystem = undefined;
+    let organizer = new SimpleOrganizerBuilder().build();
 
     t.throws(() => {
-        new TidyFoldersManager(providedFileSystem)
+        new TidyFoldersManager(providedFileSystem, organizer)
     }, "Error: Dependency 'fileSystem' was null or undefined.");
 });
 
@@ -18,8 +20,9 @@ test('it throws Error if null fileSystem', t => {
     t.plan(1);
 
     let providedFileSystem = null;
+    let organizer = new SimpleOrganizerBuilder().build();
 
     t.throws(() => {
-        new TidyFoldersManager(providedFileSystem)
+        new TidyFoldersManager(providedFileSystem, organizer)
     }, "Error: Dependency 'fileSystem' was null or undefined.");
 });
