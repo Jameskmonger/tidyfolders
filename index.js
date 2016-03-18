47101e8f8e40dcad8fcfaf9627e1f8a9df2978ee
James Monger <jameskmonger@hotmail.co.uk>
2016-03-18 15:24:12 +0000

    Instantiate objects

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var moveDir = require('fs-movedir');
 var FileSystem = require('./src/file-system').FileSystem;
 var SimpleOrganizer = require('./src/simple-organizer').SimpleOrganizer;
 var Manager = require('./src/tidyfolders-manager').TidyFoldersManager;
+
+var fileSystem = new FileSystem(moveDir);
+var organizer = new SimpleOrganizer();
+var manager = new Manager(fileSystem, organizer);
